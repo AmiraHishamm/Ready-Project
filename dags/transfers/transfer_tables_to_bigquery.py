a7505df7915ce9376aa3593e2da15908513d507d
Amira <amira.hussein@GizaSystems.onmicrosoft.com>
2025-01-08 00:46:21 +0200

    change dag name

--- dags/transfers/transfer_tables_to_bigquery.py
+++ dags/transfers/transfer_tables_to_bigquery.py
@@ -16,7 +16,7 @@ default_args = {
 }
 
 dag1 = DAG(
-    'Transfer_tables_to_bq_amira',
+    'Transfer_to_bq_amira',
     default_args=default_args,
     start_date=datetime(2025, 1, 7),
     description='Transfer postgres to gcs and from gcs to bigquery',
